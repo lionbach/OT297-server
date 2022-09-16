f7356dd34abb47740db388e783411f2c06fd3089
juanotturi <juanotturi@gmail.com>
2022-09-16 14:33:39 -0300

    CategoryEntity created

---
+++ src/main/java/com/alkemy/ong/models/entity/CategoryEntity.java
@@ -0,0 +1,39 @@
+package com.alkemy.ong.models.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE categories SET deleted = true where id=?")
+@Where(clause = "deleted=false")
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "timestamps", nullable = false)
+    @CreationTimestamp
+    private LocalDateTime timestamps;
+
+    @Column(name = "deleted", nullable = false)
+    private boolean deleted = Boolean.FALSE;
+}
