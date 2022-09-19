2469c7078a004cc9dc1778abd61a2e58a6d1d701
lionbach <leomumbach@gmail.com>
2022-09-19 19:48:20 -0300

    comment deleted

--- src/main/java/com/alkemy/ong/models/entity/MembersEntity.java
+++ src/main/java/com/alkemy/ong/models/entity/MembersEntity.java
@@ -46,20 +46,3 @@ public class MembersEntity {
     private Timestamp timestamps;
 
 }
-
-
-/*
-COMO desarrollador
-QUIERO agregar la entidad Member
-PARA representar en la implementación la estructura de datos
-
-Criterios de aceptación:
-Nombre de tabla: members. Campos:
-name: VARCHAR NOT NULL
-facebookUrl: VARCHAR NULLABLE
-instagramUrl: VARCHAR NULLABLE
-linkedinUrl: VARCHAR NULLABLE
-image: VARCHAR NOT NULL
-description: VARCHAR NULLABLE
-timestamps y softDelete
- */
