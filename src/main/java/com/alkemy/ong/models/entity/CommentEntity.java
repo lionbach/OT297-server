b6879f96e7153417e9fe3f0e95520e38ed836a49
juanotturi <juanotturi@gmail.com>
2022-10-09 23:32:15 -0300

    Comment entity, repository and service created

---
+++ src/main/java/com/alkemy/ong/models/entity/CommentEntity.java
@@ -0,0 +1,41 @@
+package com.alkemy.ong.models.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "comments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    private UserEntity user;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "news_id", insertable = false, updatable = false)
+    private NewEntity news;
+
+    @Column(name = "news_id", nullable = false)
+    private Long newsId;
+
+    @Column(name = "body", nullable = false, columnDefinition = "TEXT")
+    private String body;
+
+    public CommentEntity(Long userId, Long newsId, String body) {
+        this.userId = userId;
+        this.newsId = newsId;
+        this.body = body;
+    }
+}
